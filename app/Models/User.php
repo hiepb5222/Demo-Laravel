ae20ee055a3d187458fdd29639a3a8b21d306771
hoang hiep <1951060695@e.tlu.edu.vn>
2023-01-10 11:49:15 +0700

    create model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+   
 
     /**
      * The attributes that should be hidden for serialization.
@@ -40,5 +36,8 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+    ];
+    protected $guarded = [
+        "is_admin",
     ];
 }
