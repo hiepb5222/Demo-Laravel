c47d29c2b8f73899200182a59ee067e72be523ab
hoang hiep <1951060695@e.tlu.edu.vn>
2023-01-12 10:22:55 +0700

    create relationships on models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Student extends Model
+class Course extends Model
 {
     use HasFactory;
-
-    public function course(){
-        return $this->belongsTo(Course::class);
+    
+    public function students()
+    {
+        return $this->hasMany(Student::class);
     }
 }
